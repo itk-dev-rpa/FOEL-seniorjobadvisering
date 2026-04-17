a896a824fdc072585bed8e7525e87021df10de9e
Mathias G <ghbm@aarhus.dk>
2026-04-17 10:18:10 +0200

    Added constants and queue names

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -13,3 +13,22 @@ SCREENSHOT_SENDER = "robot@friend.dk"
 
 # Constant/Credential names
 ERROR_EMAIL = "Error Email"
+KEYVAULT_CREDENTIALS = "Keyvault"
+KEYVAULT_URI = "Keyvault URI"
+
+# Local constants
+KEYVAULT_PATH = "Digital_Post_Senioradvis"  # TODO
+MBU_MAIL_SENDER = "senioradvis@mbu.aarhus.dk"
+MSO_MAIL_SENDER = "senioradvis@mso.aarhus.dk"
+MBA_MAIL_SENDER = "senioradvis@ba.aarhus.dk"
+
+# Queues
+MBU_QUEUE_EMPLOYEE = "MBU_Senioradvisering_medarbejder"
+MBU_QUEUE_SUPERVISOR = "MBU_Senioradvisering_leder"
+
+MSO_QUEUE_EMPLOYEE = "MSO_Senioradvisering_medarbejder"
+MSO_QUEUE_SUPERVISOR = "MSO_Senioradvisering_leder"
+
+MBA_QUEUE_EMPLOYEE = "MBA_Senioradvisering_medarbejder"
+MBA_QUEUE_SUPERVISOR = "MBA_Senioradvisering_leder"
+
